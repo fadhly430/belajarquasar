2aee7c6015c43b493ed41e7b39a39a97faf87964
Muhammad fadhly Fadhly <albashiroh430@gmail.com>
2019-10-10 12:39:29 +0700

    nambah v-if dan v-else

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,9 +2,15 @@
   <q-page class="q-pa-md">
   <div class="q-gutter-md">
   <input v-model="message" @keyup = "handleKeyup"/>
-   <h1>{{message}}</h1>
+  <h1>{{message}}</h1>
+
+   <h5 v-if="message.length" class="border-black">{{message}}</h5>
+   <h6 v-else>tidak ada tulisan </h6>
    <button @click="message= ''"> hapus </button> 
-   <button @click=" clearMessage= ''"> clear dengan methods </button>
+   <button @click=" clearMessage= ''"> clear dengan fungsi </button>
+
+  
+
   </div>
   </q-page>
 </template>
@@ -14,17 +20,17 @@ export default
 {
   data() {
     return {
-      message: 'bismillah '
+      message: 'mau apa ? '
     }
   },
 
   methods: {
-    clearMessage(){
-      this.message=''
-    },
+  clearMessage(){
+  this.message=''
+  },
     handleKeyup(e) {
       if (e.keyCode == 27){
-        this.message =''
+        this.message ='ESC'
       }
     }
   }
