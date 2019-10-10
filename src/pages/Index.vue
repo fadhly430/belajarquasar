49f5486927096fa6ec61a3776b321060b7513cd9
Muhammad fadhly Fadhly <albashiroh430@gmail.com>
2019-10-10 11:03:48 +0700

    vue

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="q-pa-md">
+  <div class="q-gutter-md">
+  <input v-model="message" @keyup = "handleKeyup"/>
+   <h1>{{message}}</h1>
+   <button @click="message= ''"> hapus </button> 
+   <button @click=" clearMessage= ''"> clear dengan methods </button>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default 
+{
+  data() {
+    return {
+      message: 'bismillah '
+    }
+  },
+
+  methods: {
+    clearMessage(){
+      this.message=''
+    },
+    handleKeyup(e) {
+      if (e.keyCode == 27){
+        this.message =''
+      }
+    }
+  }
+  
+  
+}
+</script>
+
+
