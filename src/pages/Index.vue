1c1be37035f92b34215d35cff25e90cd771ea268
Muhammad fadhly Fadhly <albashiroh430@gmail.com>
2019-10-15 14:59:38 +0700

    add

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,75 +1,91 @@
 <template>
   <div class="q-pa-md">
-    <q-table
-      :data="employees"
-      :columns="columns"
-      row-key="name"
-    />
+    <q-layout view="lHh Lpr lff" container style="height: 700px" class="shadow-2 rounded-borders">
+      <q-header elevated class="bg-cyan-8">
+        <q-toolbar>
+          <q-toolbar-title>FADHLYBOOK</q-toolbar-title>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="700"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section>
+                Send
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://avatars3.githubusercontent.com/u/46949534?s=400&u=a086b9152f23b5c95afdfc5646bb2c5d9329606a&v=4">
+            </q-avatar>
+            <div class="text-weight-bold">Muhammad Fadhly</div>
+            <div>@fadhlybook</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+      <q-page-container>
+        <q-page padding>
+       bismillah
+            
+             </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   data () {
     return {
-      columns: [
-        {
-          name: 'name',
-          required: true,
-          label: 'Nama',
-          align: 'left',
-          field: row => row.nama,
-          format: val => `${val}`,
-          sortable: true,
-          classes: 'bg-grey-2 ellipsis',
-          style: 'max-width: 100px'
-        },
-        {
-          name: 'jenis_kelamin',
-          required: true,
-          label: 'Jenis Kelamin',
-          align: 'left',
-          field: row => row.jeniskelamin,
-          format: val => `${val}`,
-          sortable: true,
-          classes: 'bg-grey-2 ellipsis',
-          style: 'max-width: 100px'
-        },
-        {
-          name: 'tanggallahir',
-          required: true,
-          label: 'Tanggal Lahir',
-          align: 'left',
-          field: row => row.tanggallahir,
-          format: val => `${val}`,
-          sortable: true,
-          classes: 'bg-grey-2 ellipsis',
-          style: 'max-width: 100px'
-        },
-        {
-          name: 'status',
-          required: true,
-          label: 'Status',
-          align: 'left',
-          field: row => row.status,
-          format: val => `${val}`,
-          sortable: true,
-          classes: 'bg-grey-2 ellipsis',
-          style: 'max-width: 100px'
-        }
-        
-         ],
-      employees: [
-        {
-         
-        }
-      ]
+      drawer: false
     }
-  },
-  async mounted(){
-    const response = await axios.get('http://localhost:3000/api/Employees')
-    this.employees=response.data
   }
 }
 </script>
