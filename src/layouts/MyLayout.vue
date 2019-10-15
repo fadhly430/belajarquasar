1bd2340441623a2768a4df31b1ef21d462f122c5
Muhammad fadhly Fadhly <albashiroh430@gmail.com>
2019-10-15 11:07:54 +0700

    add

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -8,45 +8,13 @@
     <div class="q-gutter-md row">
 
   
-           <template>
-  <div class="q-pa-md q-gutter-sm">
-    <q-btn label="REGISTRASI" color="primary" @click="layout = true" />
-
-    <q-dialog v-model="layout">
-      <q-layout view="Lhh lpR fff" container class="bg-white">
-        <q-header class="bg-primary">
-          <q-toolbar>
-          
-            <q-toolbar-title>Pendaftaran</q-toolbar-title>
-            <q-btn flat v-close-popup round dense icon="close" />
-          </q-toolbar>
-        </q-header>
-
-        <q-page-container>
-          <q-page padding>
            
-            </p>
-          </q-page>
-        </q-page-container>
-      </q-layout>
-    </q-dialog>
-  </div>
-</template>
-         
-
-
     </div>
   </div>
 </template>
 
       </q-toolbar>
     </q-header>
-
- 
-    <template>
- 
-</template>
-  
     </q-drawer>
 
     <q-page-container>
@@ -60,9 +28,7 @@ export default {
   name: 'MyLayout',
   data () {
     return {
-      layout: false
-
-     
+      leftDrawerOpen: false
     }
   }
 }
